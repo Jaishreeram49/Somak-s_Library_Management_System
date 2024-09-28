debd88dde01ce39ced027979bd4a0c35f30208bf
Somak Mahapatra <somakmahapatra681@gmail.com>
2024-09-28 17:08:39 +0530

    all

---
+++ admin/profile_update.php
@@ -0,0 +1,82 @@
+<?php
+	include 'includes/session.php';
+
+	if(isset($_GET['return'])){
+		$return = $_GET['return'];
+	}
+	else{
+		$return = 'home.php';
+	}
+
+	if(isset($_POST['save'])){
+		$curr_password = $_POST['curr_password'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$photo = $_FILES['photo']['name'];
+		if(password_verify($curr_password, $user['password'])){
+			if(!empty($photo)){
+				move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$photo);
+				$filename = $photo;	
+			}
+			else{
+				$filename = $user['photo'];
+			}
+
+			if($password == $user['password']){
+				$password = $user['password'];
+			}
+			else{
+				$password = password_hash($password, PASSWORD_DEFAULT);
+			}
+
+			$sql = "UPDATE admin SET username = ?, password = ?, firstname = ?, lastname = ?, photo = ? WHERE id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssssi", $username, $password, $firstname, $lastname, $filename, $user['id']);
+
+if ($stmt->execute()) {
+    $_SESSION['success'] = 'Admin profile updated successfully';
+} else {
+    if ($return == 'borrow.php' OR $return == 'return.php') {
+        if (!isset($_SESSION['error'])) {
+            $_SESSION['error'] = array();
+        }
+        $_SESSION['error'][] = $stmt->error;
+    } else {
+        $_SESSION['error'] = $stmt->error;
+    }
+}
+
+			
+		}
+		else{
+			if($return == 'borrow.php' OR $return == 'return.php'){
+				if(!isset($_SESSION['error'])){
+					$_SESSION['error'] = array();
+				}
+				$_SESSION['error'][] = 'Incorrect password';
+			}
+			else{
+				$_SESSION['error'] = 'Incorrect password';
+			}
+
+		}
+	}
+	else{
+		if($return == 'borrow.php' OR $return == 'return.php'){
+			if(!isset($_SESSION['error'])){
+				$_SESSION['error'] = array();
+			}
+			$_SESSION['error'][] = 'Fill up required details first';
+		}
+		else{
+			$_SESSION['error'] = 'Fill up required details first';
+		}
+		
+	}
+
+	header('location:'.$return);
+
+?>
